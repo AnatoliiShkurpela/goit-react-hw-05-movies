bbd38947964f6eb6876d88692006197b9646635b
AnatoliiShkurpela <toha.aladin@gmail.com>
2023-06-26 21:55:03 +0300

    new

--- src/pages/Movies/Movies.jsx
+++ src/pages/Movies/Movies.jsx
@@ -26,7 +26,7 @@ function Movies() {
           {movies.results.map(result => {
             return (
               <MoviesTitle key={result.id}>
-                <MoviesItem to={`${result.id}`} state={location.state}>
+                <MoviesItem to={`${result.id}`} >
                   <p>{result.original_title}</p>
                 </MoviesItem>
               </MoviesTitle>
